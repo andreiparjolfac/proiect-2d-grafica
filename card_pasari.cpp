6da721432abf009d5fad1df4b0f28b0ca05cedb5
andreiparjolfac <119656860+andreiparjolfac@users.noreply.github.com>
2023-12-13 16:45:20 +0200

    Update card_pasari.cpp

--- card_pasari.cpp
+++ card_pasari.cpp
@@ -2,20 +2,36 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
-#include<windows.h>
+#include <windows.h>
 #include <GL/freeglut.h>
 #include <stdlib.h>
-#include < random >
+#include <random>
+#include <SOIL.h>
 using namespace std;
 
 double screenX = 800.0;
 double screenY = 600.0;
 
+double viteza_animatie = 1.0 / 50.0;
+static GLdouble i = 0.0;
+static GLdouble j = 0.0;
+static GLdouble h = 0.0;
+static GLdouble alpha = 0.1;
+static GLdouble step = 0.1;
+static GLdouble rotationAngle = 0.0f;
 
+static GLdouble destX;
+static GLdouble destY;
+
+static GLdouble dx;
+static GLdouble dy;
+
+GLuint texturaID, fundalTexturaID;
 
 class Pasare {
 public:
 	double offsetx, offsety;
+	boolean zboara = false;
 	Pasare(double spawnX, double spawnY) {
 		offsetx = spawnX;
 		offsety = spawnY;
@@ -23,29 +39,31 @@ public:
 
 	void deseneaza_pasare(void) {
 
-		glColor3d(0.0, 0.0, 0.0);
 
-		glBegin(GL_POLYGON);
-		glVertex2i(offsetx + 15, offsety + 20);
-		glVertex2i(offsetx + 15, offsety + 15);
-		glVertex2i(offsetx + 5, offsety);
-		glVertex2i(offsetx, offsety);
+		if (zboara) {
+			offsetx += dx;
+			offsety += dy;
+			glTranslated(i, j, 0.0);
+			glTranslated(offsetx + 20, offsety + 20, 0.0);
+			glRotated(rotationAngle, 0.0, 0.0, 1.0);
+			glTranslated(-offsetx - 20, -offsety - 20, 0.0);
+		}
+
+		glColor3f(1.0, 1.0, 1.0);
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, texturaID);
+
+
+		glBegin(GL_QUADS);
+		glTexCoord2f(0.0, 0.0); glVertex2i(offsetx, offsety);
+		glTexCoord2f(1.0, 0.0); glVertex2i(offsetx + 40, offsety);
+		glTexCoord2f(1.0, 1.0); glVertex2i(offsetx + 40, offsety + 40);
+		glTexCoord2f(0.0, 1.0); glVertex2i(offsetx, offsety + 40);
 		glEnd();
 
-		glBegin(GL_POLYGON);
-		glVertex2i(offsetx + 15, offsety + 15);
-		glVertex2i(offsetx + 20, offsety + 15);
-		glVertex2i(offsetx + 20, offsety + 20);
-		glVertex2i(offsetx + 15, offsety + 20);
-		glEnd();
+		glLoadIdentity();
+		glDisable(GL_TEXTURE_2D);
 
-		glBegin(GL_POLYGON);
-		glVertex2i(offsetx + 20, offsety + 15);
-		glVertex2i(offsetx + 35, offsety);
-		glVertex2i(offsetx + 40, offsety);
-		glVertex2i(offsetx + 20, offsety + 20);
-		glEnd();
-	
 	}
 };
 
@@ -62,36 +80,131 @@ public:
 		coordX = raza_card * (1 - rx) + rx * (screenX - raza_card);
 		coordY = raza_card * (1 - ry) + ry * (screenY - raza_card);
 		for (int i = 0; i < nr_pasari; i++) {
-			rx = 2* (dis(generator)) - 1;
-			ry = 2* (dis(generator)) - 1;
-			pasari.push_back(new Pasare( coordX + rx*raza_card, coordY+ry*raza_card));
+			rx = 2 * (dis(generator)) - 1;
+			ry = 2 * (dis(generator)) - 1;
+			pasari.push_back(new Pasare(coordX + rx * raza_card, coordY + ry * raza_card));
 		}
+	}
+	void zboara_la(GLdouble X, GLdouble Y) {
+		destX = X;
+		destY = Y;
+		for (int i = 0; i < pasari.size(); i++) {
+			pasari[i]->zboara = true;
+		}
+
 	}
 };
 
-Card c1(7, 30);
+Card c1(7, 60);
 
 void init(void)
 {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
-	gluOrtho2D(0.0,screenX, 0.0,screenY);
+	gluOrtho2D(0.0, screenX, 0.0, screenY);
+	texturaID = SOIL_load_OGL_texture("pasareSus.png", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+	fundalTexturaID = SOIL_load_OGL_texture("fundal.bmp", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 }
+
+void deseneazaFundal()
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, fundalTexturaID);
+
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 0); glVertex2f(0, 0);
+	glTexCoord2f(1, 0); glVertex2f(screenX, 0);
+	glTexCoord2f(1, 1); glVertex2f(screenX, screenY);
+	glTexCoord2f(0, 1); glVertex2f(0, screenY);
+	glEnd();
+
+	//glDisable(GL_TEXTURE_2D);
+}
+
 
 void deseneaza(void)
 {
+
+
+
+
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glMatrixMode(GL_MODELVIEW);
 
-	for (int i = 0; i < c1.pasari.size(); i++) {
+	deseneazaFundal();
+	glLoadIdentity();
+
+	for (int i = 0; i < c1.pasari.size(); i++) {		
+
+
 		c1.pasari[i]->deseneaza_pasare();
+		cout << "as";
 	}
+
 
 
 	glPopMatrix();
 	glutSwapBuffers();
 
 	glFlush();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, screenX, 0.0, screenY, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+void miscas(void)
+{
+	dy = ((destY - c1.coordY) / (destX - c1.coordX) + 5*cos(i/3)) * viteza_animatie;
+	if (c1.coordX < destX) {
+		dx = viteza_animatie;
+		
+	}else{
+		dx = -viteza_animatie;
+		dy = -dy;
+	}
+	
+
+	rotationAngle += 0.02f;
+	if (rotationAngle > 360.0f)
+		rotationAngle -= 360.0f;
+	
+
+	
+	if (abs(c1.coordX+i - destX) < 10 and abs(c1.coordY+j - destY) < 10) {
+		dx = 0;
+		dy = 0;
+		cout << c1.coordX + i << endl;
+		cout << c1.coordY + j << endl;
+		cout << "am ajuns";
+	}
+	else {
+		i = i + dx;
+		j = j + dy;
+	}
+
+	glutPostRedisplay();
+}
+void mouse(int button, int state, int x, int y)
+{
+	switch (button) {
+	case GLUT_LEFT_BUTTON:
+		if (state == GLUT_DOWN)
+			c1.zboara_la(600, 600);
+			glutIdleFunc(miscas);
+		break;
+	default:
+		break;
+	}
 }
 
 void main(int argc, char** argv)
@@ -104,21 +217,7 @@ void main(int argc, char** argv)
 	glutCreateWindow("Card de Pasari");
 	init();
 	glutDisplayFunc(deseneaza);
-
-	//glutMouseFunc(mouse);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
 	glutMainLoop();
 }
-/*
-
-int main() {
-
-	srand(time(NULL));
-	cout << "coordonatele cardului" << endl;
-	cout << c1.coordX << " " << c1.coordY << endl;
-	for (int i = 0; i < c1.pasari.size(); i++) {
-		cout << c1.pasari[i]->offsetx<<"  " << c1.pasari[i]->offsety<<endl;
-	}
-	return 0;
-
-}
-*/
